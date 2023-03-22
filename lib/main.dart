94ad59155cad5fb087fedc719c69efa20cc8c3dd
YogeshRajgure <noobcoderyogesh.vraj@gmail.com>
2023-03-22 15:11:40 +0530

    flutter setup

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyShopApp'),
+      ),
+      body: Center(child: Text("lets build this app")),
+    );
+  }
+}
