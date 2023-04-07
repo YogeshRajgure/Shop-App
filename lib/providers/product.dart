1097b808e3040a5667a6a33fd3d4fa27edfb652a
YogeshRajgure <noobcoderyogesh.vraj@gmail.com>
2023-04-07 23:56:37 +0530

    favourite update on server

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +20,33 @@ class Product with ChangeNotifier {
     this.isFavourite = false,
   });
 
-  void toggleFavouriteStatus() {
+  void _setFavValue(bool newValue) {
+    isFavourite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavouriteStatus() async {
+    final old_status = isFavourite;
     isFavourite = !isFavourite;
     notifyListeners();
+    final url = Uri.https(
+      'shopapp-cd604-default-rtdb.firebaseio.com',
+      '/products/$id',
+    );
+    try {
+      final response = await http.patch(url,
+          body: json.encode({
+            'isFavourite': isFavourite,
+          }));
+      if (response.statusCode >= 400) {
+        // isFavourite = old_status;
+        // notifyListeners();
+        _setFavValue(old_status);
+      }
+    } catch (error) {
+      // isFavourite = old_status;
+      // notifyListeners();
+      _setFavValue(old_status);
+    }
   }
 }
